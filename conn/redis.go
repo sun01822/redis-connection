8b51292e1aaa1c247d74093251a030bf89d7a744
MD. Shariar Hossain Sun <mdshariarhossainsun01822@gmail.com>
2024-10-20 13:14:32 +0600

    set and get value from redis

--- conn/redis.go
+++ conn/redis.go
@@ -1,9 +1,11 @@
 package conn
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/go-redis/redis"
 	"github.com/sun01822/redis-connection/config"
-	"fmt"
 )
 
 
@@ -12,7 +14,7 @@ var client *redis.Client
 func ConnectRedis(){
 	conf := config.Redis()
 
-	fmt.Print("connecting to redis at ", conf.Host, ":" + conf.Port, "...")
+	fmt.Println("connecting to redis at ", conf.Host, ":" + conf.Port, "...")
 
 	client = redis.NewClient(&redis.Options{
 		Addr:     conf.Host + ":" + conf.Port,
@@ -21,9 +23,31 @@ func ConnectRedis(){
 	})
 
 	if _, err := client.Ping().Result(); err != nil {
-		fmt.Print("redis connection error: ", err)
+		fmt.Println("redis connection error: ", err)
 		panic(err)
     } 
 	
-	fmt.Print("redis connected successfully")
+	fmt.Println("redis connected successfully")
+}
+
+type RedisClient struct {}
+
+func NewRedisClient() *RedisClient {
+	return &RedisClient{}
+}
+
+func (c *RedisClient) Get(key string) (string, error) {
+    return client.Get(key).Result()
+}
+
+func (c *RedisClient) Set(key string, value string, expiration time.Duration) error {
+    return client.Set(key, value, expiration).Err()
+}
+
+func (c *RedisClient) Del(keys ...string) (int, error) {
+    err := client.Del(keys...)
+	if err!= nil {
+        return 0, fmt.Errorf("error deleting")
+    }
+	return len(keys), nil
 }
--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"github.com/sun01822/redis-connection/config"
 	"github.com/sun01822/redis-connection/conn"
+	"fmt"
 )
 
 func main() {
 	config.LoadConfig()
 	conn.ConnectRedis()
+
+	client := conn.NewRedisClient()
+
+	err := client.Set("appKey", "ea08dcb5-82ef-4738-bff4-3e3604b4d1f5", 0)
+	if err!= nil {
+        panic(err)
+    }
+
+	value, err := client.Get("appKey")
+	if err!= nil {
+        panic(err)
+    }
+	fmt.Println("Get AppKey: ", value)
 }
